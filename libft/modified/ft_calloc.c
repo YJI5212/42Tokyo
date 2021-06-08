ce53af676bf00070a4457619d0f0f4851b75be09
YJI5212 <y.inoue.5212@gmail.com>
2021-06-08 21:25:55 +0900

    libft

---
+++ libft/modified/ft_calloc.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yoinoue <yoinoue@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/22 22:29:36 by yoinoue           #+#    #+#             */
+/*   Updated: 2021/06/08 21:22:01 by yoinoue          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t num, size_t size)
+{
+	void	*ptr;
+
+	if (num == 0 || size == 0)
+	{
+		num = 1;
+		size = 1;
+	}
+	ptr = (void *)malloc(num * size);
+	if (ptr)
+		ft_bzero(ptr, num * size);
+	return (ptr);
+}
---
+++ libft/modified/ft_lstclear.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yoinoue <yoinoue@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/10 00:26:37 by yoinoue           #+#    #+#             */
+/*   Updated: 2020/07/10 02:53:59 by yoinoue          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*p;
+	t_list	*tmp;
+
+	if (!lst || !del)
+		return ;
+	p = *lst;
+	while (p)
+	{
+		tmp = p->next;
+		ft_lstdelone(p, del);
+		p = tmp;
+	}
+	*lst = NULL;
+}
