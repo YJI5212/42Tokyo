edb7cbd2794e9e4e9ba5296d28597cec50cd2412
YJI5212 <y.inoue.5212@gmail.com>
2021-06-22 10:47:33 +0900

    client_pid 1 leak modified

--- minitalk/c_srcs/main.c
+++ minitalk/c_srcs/main.c
@@ -10,10 +10,13 @@ void	check_args(int argc, char *argv[])
 int	main(int argc, char *argv[])
 {
 	t_client	info;
+	char		*head;
 
 	check_args(argc, argv);
 	init_client(&info, argv);
+	head = info.message;
 	send_message(&info);
+	free(head);
 	info.message = argv[2];
 	send_message(&info);
 	return (EXIT_SUCCESS);
